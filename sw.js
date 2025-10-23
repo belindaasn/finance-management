a9c6a658087b213e5004a9be89ecc939aa169305
bel <169089020+belindaasn@users.noreply.github.com>
2025-10-23 08:40:53 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,43 @@
-const CACHE_NAME = 'finance-manager-v1';
+const CACHE_NAME = 'finance-id-v3';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/script.js',
-    '/manifest.json'
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json',
+    './icon-192.png',
+    './icon-512.png'
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(function(cache) {
+            .then(cache => {
                 return cache.addAll(urlsToCache);
             })
+            .then(() => self.skipWaiting())
     );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-            .then(function(response) {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            }
-        )
+            .then(response => {
+                return response || fetch(event.request);
+            })
     );
 });
